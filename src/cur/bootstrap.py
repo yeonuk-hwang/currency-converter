365abb57670ecf1923438042b5e19f3150d059a3
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-11-06 00:08:26 +1030

    feat: add environment variable support to cache directory
    If env varaible exists, the env variable take priority over default
    cache directory

--- src/cur/bootstrap.py
+++ src/cur/bootstrap.py
@@ -1,9 +1,18 @@
+import os
+from pathlib import Path
+
+from cur.adapters.cache import ExchangeRateCache, FileCacheStorage
 from cur.adapters.exchange_rate_client import ExchangeRateClient
 from cur.services.conversion import ConversionService
 
 
 def bootstrap() -> ConversionService:
-    exchange_rate_client = ExchangeRateClient()
+    cache_dir_env = os.getenv("CURRENCY_TRANSLATOR_CACHE_DIR")
+    cache_dir = Path(cache_dir_env) if cache_dir_env else None
+
+    storage = FileCacheStorage(cache_dir)
+    cache = ExchangeRateCache(storage)
+    exchange_rate_client = ExchangeRateClient(cache)
     conversion_service = ConversionService(exchange_rate_client)
 
     return conversion_service
